81574ec4ab4936e706c84b8f9c37b7f09a994cab
TomJavaProg <javaprogtom@gmail.com>
2025-10-23 20:59:30 -0300

    Início da aplicação.

---
+++ src/main/java/com/tomazcunha/profinancnab/ProfinancnabApplication.java
@@ -0,0 +1,13 @@
+package com.tomazcunha.profinancnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfinancnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfinancnabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomazcunha/profinancnab/ProfinancnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomazcunha.profinancnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfinancnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
